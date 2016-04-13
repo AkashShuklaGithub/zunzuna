67bd58115718b58a325855e2b3e08a3e83ead2ff
Vaibhav Awachat <awachat11vaibhav@gmail.com>
2016-04-13 09:41:02 +0530

    bugfix: compare hours & minutes of timestamp (not seconds)

--- zunzuna.js
+++ zunzuna.js
@@ -9,8 +9,8 @@ var event = algo.init("12.920397, 77.685605", "12.915002, 77.663941", moment().a
 // algo.getWaitingTime("12.920397,77.685605");
 refreshInterval = setInterval(function() {
 	event.then(function(event){
-		if(moment(event.updateTravelTimeAt).isBefore(moment())){
-			console.log("Google ==>  updateTravelTimeAt: " + moment(event.updateTravelTimeAt).format("HH:mm")+ " updateWaitingTimeAt: " + moment(event.updateWaitingTimeAt).format("HH:mm") + " waitingTime: " + event.waitingTime + ", currentTime: " +  moment().format("HH:mm") + ", notificationTime: " + moment(event.notificationTime).format("HH:mm") + ", startTravelAt: " + moment(event.startTravelAt).format("HH:mm")+ ", requestedAt: " + moment(event.requestedAt).format("HH:mm"));
+		if(moment(event.updateTravelTimeAt).isBefore(moment(), 'minute')){
+			log(event, " Google ");
 			event = algo.updateEvent(event);
 		}
 		else{
@@ -18,8 +18,8 @@ refreshInterval = setInterval(function() {
 		}
 		return event;
 	}).then(function(event) {
-		if(moment(event.updateWaitingTimeAt).isBefore(moment())){
-			console.log("Google ==>  updateTravelTimeAt: " + moment(event.updateTravelTimeAt).format("HH:mm")+ " updateWaitingTimeAt: " + moment(event.updateWaitingTimeAt).format("HH:mm") + " waitingTime: " + event.waitingTime + ", currentTime: " +  moment().format("HH:mm") + ", notificationTime: " + moment(event.notificationTime).format("HH:mm") + ", startTravelAt: " + moment(event.startTravelAt).format("HH:mm")+ ", requestedAt: " + moment(event.requestedAt).format("HH:mm"));
+		if(moment(event.updateWaitingTimeAt).isBefore(moment(), 'minute')){
+			log(event, " Uber ");
 			event = algo.updateEventWithUber(event);
 		}
 		else{
@@ -27,13 +27,21 @@ refreshInterval = setInterval(function() {
 		}
 		return event;
 	}).then(function(event){
-		if( moment(event.notificationTime).isBefore(moment())){
+		if( moment(event.notificationTime).isBefore(moment(), 'minute')){
 			console.log("====================================")
-			console.log("|==> notificationTime: " + moment(event.notificationTime).format("HH:mm") + ", currentTime: " + moment().format("HH:mm"));
+			console.log("|==> notificationTime: " + moment(event.notificationTime).format("HH:mm:ss") + ", currentTime: " + moment().format("HH:mm:ss"));
 			algo.sendNotification(event.email);
 			clearInterval(refreshInterval);
 			console.log("====================================")
 		}
+	}).fail(function() {
+		console.log("Failed. Try Again.")
+		process.exit(1);
 	}).done();
 }, 10000);
+
+var log = function(event, type){
+	process.stdout.write("\n")
+	console.log(type +" ==>  updateTravelTimeAt: " + moment(event.updateTravelTimeAt).format("HH:mm:ss")+ " updateWaitingTimeAt: " + moment(event.updateWaitingTimeAt).format("HH:mm:ss") + " waitingTime: " + event.waitingTime + ", currentTime: " +  moment().format("HH:mm:ss") + ", notificationTime: " + moment(event.notificationTime).format("HH:mm:ss") + ", startTravelAt: " + moment(event.startTravelAt).format("HH:mm:ss")+ ", requestedAt: " + moment(event.requestedAt).format("HH:mm:ss"));
+}
 
