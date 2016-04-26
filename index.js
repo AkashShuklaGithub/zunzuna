f1c82fd6108c923e6624e89f7fb55d0e8051e0d8
Vaibhav Awachat <awachat11vaibhav@gmail.com>
2016-04-26 23:11:16 +0530

    add eventEmitter

--- index.js
+++ index.js
@@ -1,50 +1,85 @@
+'user strict'
+
 var algo = require('./lib/algo');
 var moment = require('moment');
+var events = require('events');
 
 moment().format();
 
-function createEvent(event) {
-    var event = algo.init(event.source, event.destination, event.time, event.emailid);
+const debug = true;
+const REFRESH_INTERVAL = 10000;
+const validateEvent = (event) => {
+    if (typeof event !== 'object') {
+        throw new Error("Event must be an object");
+    }
+    if (!event.source) {
+        throw new Error("The 'source' is missing.");
+    }
+    if (!event.destination) {
+        throw new Error("The 'destination' is missing.");
+    }
+    if (!event.time) {
+        throw new Error("The 'time' is missing.");
+    }
+    if (!event.emailid) {
+        event.emailid = null;
+    }
+    return event;
+};
 
-    // set the interval
-    var refreshInterval = setInterval(function() {
-        event.then(function(event) {
-            if (moment(event.updateTravelTimeAt).isBefore(moment(), 'minute')) {
-                log(event, " Google ");
-                event = algo.updateEvent(event);
-            } else {
-                process.stdout.write("g")
-            }
-            return event;
-        }).then(function(event) {
-            if (moment(event.updateWaitingTimeAt).isBefore(moment(), 'minute')) {
-                log(event, " Uber ");
-                event = algo.updateEventWithUber(event);
-            } else {
-                process.stdout.write("u")
-            }
-            return event;
-        }).then(function(event) {
-            if (moment(event.notificationTime).isBefore(moment(), 'minute')) {
-                console.log("====================================")
-                console.log("|==> notificationTime: " + moment(event.notificationTime).format("HH:mm:ss") + ", currentTime: " + moment().format("HH:mm:ss"));
-                algo.sendNotification(event.email);
+const zunzuna = function() {
+    // todo List of subscribers
+    // let subscribers = [{subcriberid: ##, eventid:##}];
+    events.EventEmitter.call(this);
+
+    this.createEvent = (event) => {
+        var event = validateEvent(event);
+        event = algo.init(event.source, event.destination, event.time, event.emailid);
+        var self = this;
+        // set the interval
+        var refreshInterval = setInterval(function() {
+            event.then(function(event) {
+                if (moment(event.updateTravelTimeAt).isBefore(moment(), 'minute')) {
+                    log(event, " Google ");
+                    event = algo.updateEvent(event);
+                } else {
+                    if (debug)
+                        process.stdout.write("g")
+                }
+                return event;
+            }).then(function(event) {
+                if (moment(event.updateWaitingTimeAt).isBefore(moment(), 'minute')) {
+                    log(event, " Uber ");
+                    event = algo.updateEventWithUber(event);
+                } else {
+                    if (debug)
+                        process.stdout.write("u")
+                }
+                return event;
+            }).then(function(event) {
+                if (moment(event.notificationTime).isBefore(moment(), 'minute')) {
+                    algo.log("====================================")
+                    algo.log("|==> notificationTime: " + moment(event.notificationTime).format("HH:mm:ss") + ", currentTime: " + moment().format("HH:mm:ss"));
+                    clearInterval(refreshInterval);
+                    self.emit("remind", "Time to Book Uber!");
+                    algo.log("====================================")
+                }
+            }).fail(function() {
                 clearInterval(refreshInterval);
-                console.log("====================================")
-            }
-        }).fail(function() {
-            clearInterval(refreshInterval);
-            console.log("Event is failed. Try Again.");
-        }).done();
-    }, 10000);
+                algo.log("Event is failed. Try Again.");
+            }).done();
+        }, REFRESH_INTERVAL);
+    }
 }
+
+zunzuna.prototype.__proto__ = events.EventEmitter.prototype;
 
 
 var log = function(event, type) {
     process.stdout.write("\n")
-    console.log(type + " ==>  updateTravelTimeAt: " + moment(event.updateTravelTimeAt).format("HH:mm:ss") + " updateWaitingTimeAt: " + moment(event.updateWaitingTimeAt).format("HH:mm:ss") + " waitingTime: " + event.waitingTime + ", currentTime: " + moment().format("HH:mm:ss") + ", notificationTime: " + moment(event.notificationTime).format("HH:mm:ss") + ", startTravelAt: " + moment(event.startTravelAt).format("HH:mm:ss") + ", requestedAt: " + moment(event.requestedAt).format("HH:mm:ss"));
+    algo.log(type + " ==>  updateTravelTimeAt: " + moment(event.updateTravelTimeAt).format("HH:mm:ss") + " updateWaitingTimeAt: " + moment(event.updateWaitingTimeAt).format("HH:mm:ss") + " waitingTime: " + event.waitingTime + ", currentTime: " + moment().format("HH:mm:ss") + ", notificationTime: " + moment(event.notificationTime).format("HH:mm:ss") + ", startTravelAt: " + moment(event.startTravelAt).format("HH:mm:ss") + ", requestedAt: " + moment(event.requestedAt).format("HH:mm:ss"));
 }
 
 module.exports = {
-    createEvent: createEvent
+    zunzuna: zunzuna
 };
