999251c150b45a0939c0c2766045b3ce56844152
Vaibhav Awachat <awachat11vaibhav@gmail.com>
2016-05-04 18:50:55 +0530

    udpate location in exmaple/template.js

--- examples/template.js
+++ examples/template.js
@@ -4,8 +4,8 @@ const zunzuna = new Zunzuna();
 
 const event = {
 	"id" : "1234",
-    "source": "12.925006, 76.663978",
-    "destination": "12.481734, 76.657222",
+    "source": "Bengaluru Airport",
+    "destination": "Soul Space Spirit, Bengaluru",
     "time":  "2016-05-04T02:33:17+05:30",
     "emailid": "test@test.com"
 };
